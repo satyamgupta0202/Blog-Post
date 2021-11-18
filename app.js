f7b55650f2eaed2213047cc05d87d3a9477ddc45
satyam <2019335@iiitdmj.ac.in>
2021-11-18 12:18:20 +0530

    All blogs

--- app.js
+++ app.js
@@ -1,35 +1,66 @@
+const Blog = require("./models/blog");
 const express = require("express");
-
+const mongoose = require("mongoose");
 // express app
 const app = express();
 
+const mongoURI =
+  "mongodb+srv://satyam1234:@nodenuts.h8bsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoURI)
+  .then((res) => app.listen(3000))
+  .catch((error) => console.log(error));
 // listen for requests
-app.listen(3000);
 
 // register view engine
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
 
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "name",
+//     snippet: "ok",
+//     body: "i dont know",
+//   });
+//   blog
+//     .save()
+//     .then((result) => res.send(result))
+//     .catch((err) => console.log(err));
+// });
+
+// app.get("/", (req, res) => {
+//   const blogs = [
+//     {
+//       title: "Yoshi finds eggs",
+//       snippet: "Lorem ipsum dolor sit amet consectetur",
+//     },
+//     {
+//       title: "Mario finds stars",
+//       snippet: "Lorem ipsum dolor sit amet consectetur",
+//     },
+//     {
+//       title: "How to defeat bowser",
+//       snippet: "Lorem ipsum dolor sit amet consectetur",
+//     },
+//   ];
+//   res.render("index", { title: "Home", blogs });
+// });
+
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/blogs/create", (req, res) => {
---
+++ models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchama = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("blogs", blogSchama);
+
+module.exports = Blog;
